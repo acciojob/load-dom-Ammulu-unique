0df026c29c4dc9ad1e659a5d9223324b082cf9c2
Ammulu vastupula <148691883+Ammulu-unique@users.noreply.github.com>
2026-05-23 22:21:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded",()=>{
-	const heading=document.createElement("h1");
-	heading.textContent="DOM load success";
-	document.body.appendChild(heading);
+	document.body.innerText="DOM load success";
 })
