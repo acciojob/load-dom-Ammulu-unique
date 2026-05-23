f2356e32d686eb2507898266ca39d153f00601dd
Ammulu vastupula <148691883+Ammulu-unique@users.noreply.github.com>
2026-05-23 22:07:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let body=document.getElementById("body");
+let heading=document.createElement("h1");
+heading.textContent="DOM load success";
+body.appendChild(heading);
